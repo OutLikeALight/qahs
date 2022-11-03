c02fc68756042e5fb593d3de5036a814a9e99ad2
gsdn <zehuazehua@outlook.com>
2022-11-03 09:32:37 +0800

    第一次提交

---
+++ src/main/java/com/zehua/qahs_0/Qahs0Application.java
@@ -0,0 +1,13 @@
+package com.zehua.qahs_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qahs0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Qahs0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zehua/qahs_0/Qahs0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zehua.qahs_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qahs0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
